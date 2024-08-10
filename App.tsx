9b967c7eb20400a3c47143edf50b4f75bc01f560
tacheraSasi <tacherasasi@gmail.com>
2024-08-10 21:20:07 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello this is Tach</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # snake-game-react-native
+# snake-game-react-native
